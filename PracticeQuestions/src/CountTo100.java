82cf9de6209764d44bab193d2414c15651c006c0
adamclarke888 <adamclarke88@gmail.com>
2018-12-07 09:33:07 +0000

    Created project for practice questions

---
+++ PracticeQuestions/src/CountTo100.java
@@ -0,0 +1,14 @@
+
+public class CountTo100 {
+
+	public static void main(String[] args) {
+		
+		int number = 100;
+		
+		for(int i = 1; i <= number; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
